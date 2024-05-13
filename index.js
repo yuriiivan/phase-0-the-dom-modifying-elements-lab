210d460b03d3dde1ceb9b3b7c53654a7c216717f
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-13 16:40:01 -0400

    Add element to DOM

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.querySelector('#main').remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = `Yuriy is the champion`
